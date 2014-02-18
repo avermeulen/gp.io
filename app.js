fb35bef874fdc19ceb33e42ed79499e2a500c52a
Andre Vermeulen <andre.vermeulen@gmail.com>
2014-02-18 23:08:19 +0000

    start moving prediction logic into PredictionManager

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ gpApp.service("predictionDataService", function(){
 			{ id:3, name : "French Grandpree"},
 			{ id:4, name : "German Grandpree"},
 		],
-
 		drivers : [
 			{ id : "1", name : "Driver 1", selected : false},
 			{ id : "2", name : "Driver 2", selected : false},
@@ -23,7 +22,31 @@ gpApp.service("predictionDataService", function(){
 	};
 });
 
-gpApp.controller("PredictionCtrl", function StoryCtrl($scope, predictionDataService){
+gpApp.service("predictionManager", function(){
+	var predictionPointsManager = {};
+
+	predictionPointsManager.selectDriver = function(driver, predictions){
+
+		var findDriver = function(prediction) { 
+			return prediction.driverName === driver.name
+		};
+
+		var existPredictionEntry = _.find(predictions, findDriver);
+		if (driver.selected === "true" && existPredictionEntry === undefined){
+			var predictionEntry = {driverName: driver.name, 
+				points : {grid : 0, podium : 0, retire: 0}, selected: false, totalPoints:0};
+			predictions.push(predictionEntry);
+		}
+		else if (driver.selected === "false" && existPredictionEntry !== undefined){
+			_.remove(predictions, findDriver);
+		}
+
+	};
+
+	return predictionPointsManager;
+});
+
+gpApp.controller("PredictionCtrl", function StoryCtrl($scope, predictionDataService, predictionManager){
 
 	$scope.races = predictionDataService.races;
 	$scope.drivers = predictionDataService.drivers;
@@ -36,8 +59,7 @@ gpApp.controller("PredictionCtrl", function StoryCtrl($scope, predictionDataServ
 		var race = $scope.races[$scope.selectedRaceId];
 		var raceName = race.name;
 		$scope.racePrediction.name = raceName;
-		//alert($scope.racePrediction );
-		console.log(JSON.stringify($scope.racePrediction));
+		//console.log(JSON.stringify($scope.racePrediction));
 		return raceName;
 	};
 
@@ -49,17 +71,7 @@ gpApp.controller("PredictionCtrl", function StoryCtrl($scope, predictionDataServ
 	$scope.predictionTypes = ["grid", "retire", "podium"];
 
 	$scope.driverSelectionChanged = function(driver){
-
-		var findDriver = function(prediction) { return prediction.driverName === driver.name};
-		var existPredictionEntry = _.find($scope.racePrediction.predictions, findDriver);
-		if (driver.selected === "true" && existPredictionEntry === undefined){
-			var predictionEntry = {driverName: driver.name, 
-				points : {grid : 0, podium : 0, retire: 0}, selected: false, totalPoints:0};
-			$scope.racePrediction.predictions.push(predictionEntry);
-		}
-		else if (driver.selected === "false" && existPredictionEntry !== undefined){
-			_.remove($scope.racePrediction.predictions, findDriver);
-		}
+		predictionManager.selectDriver(driver, $scope.racePrediction.predictions);
 	};
 
 	$scope.totalPoints = function(){
@@ -70,35 +82,33 @@ gpApp.controller("PredictionCtrl", function StoryCtrl($scope, predictionDataServ
 			function(t, pred) {
 				return t + pred.totalPoints
 			}, 0);
-
 		return total !== undefined ? total : 0;
 	};
 
 	$scope.pointsPerCategory = function(){
-		return _.reduce($scope.racePrediction.predictions, function(initialPoints, 
-			prediction){
-			var points = prediction.points;
-			initialPoints.grid += points.grid ? points.grid : 0;
-			initialPoints.retire += points.retire ? points.retire : 0;
-			initialPoints.podium += points.podium ? points.podium : 0;
-			return initialPoints;
-		}, {grid : 0, podium : 0, retire: 0});
-	}
+		return _.reduce(
+			$scope.racePrediction.predictions, 
+			function(initialPoints, prediction){
+				var points = prediction.points;
+				initialPoints.grid += points.grid ? points.grid : 0;
+				initialPoints.retire += points.retire ? points.retire : 0;
+				initialPoints.podium += points.podium ? points.podium : 0;
+				return initialPoints;
+			}, {grid : 0, podium : 0, retire: 0});
+	};
 
 	$scope.increase = function(prediction, field){
 		if ($scope.maxPointsUsed())
 			return;
-
 		prediction.points[field] = prediction.points[field] + 1;
 		prediction.totalPoints +=1;
-	}
+	};
 
 	$scope.decrease = function(prediction, field){
 		if (prediction.points[field] === 0)
 			return;
 		prediction.points[field] = prediction.points[field] - 1;
 		prediction.totalPoints -=1;
-	}
+	};
 
 });
-
